94f0c0af11bc402d3e62131aef5b3cf493212626
zhangwanping <zhangwanping@tpson.cn>
2021-08-09 15:26:07 +0800

    Add: 登录UI

--- app/src/main/java/com/example/videoapp/MainActivity.java
+++ app/src/main/java/com/example/videoapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.videoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.videoapp.activtiy.LoginActivtiy;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.btn_register);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivtiy.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/videoapp/activtiy/LoginActivtiy.java
@@ -0,0 +1,17 @@
+package com.example.videoapp.activtiy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.videoapp.R;
+
+public class LoginActivtiy extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activtiy);
+    }
+}
